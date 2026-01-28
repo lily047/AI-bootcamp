dc92e025002ecbcb59ef29df74071e78a34569e7
lily047 <meghanaballi07@gmail.com>
2026-01-28 20:11:31 +0530

    create base app on app.py

--- fast-api_app/app.py
+++ fast-api_app/app.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI 
+from pydantic import BaseModel 
 
+app = FastAPI()
+
+class Student(BaseModel): #used for automatic schema validation 
+    name : str 
+    age: int
+    course: str
+
+@app.get('/')
+def home():
+    return "Hello Welcome"
+
+@app.post('/student/create')
+def student(data: Student): 
+    name = data.name 
